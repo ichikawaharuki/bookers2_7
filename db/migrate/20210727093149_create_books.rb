37948dce919d9e000c7735707054c6e65510c539
EC2 Default User <ec2-user@ip-172-31-29-4.ap-northeast-1.compute.internal>
2023-09-16 04:27:52 +0000

    7d

--- db/migrate/20210727093149_create_books.rb
+++ db/migrate/20210727093149_create_books.rb
@@ -6,6 +6,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
       t.integer :user_id
       t.integer :favorites_count
       t.integer :views_count, default: 0
+      t.string :star
       t.timestamps
     end
   end
